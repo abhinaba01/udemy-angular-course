7086923ff8f117831636591ff1b4a049bc79118f
Abhinaba Mandal <abhinabamandal2001@gmail.com>
2025-02-04 16:17:06 +0530

    Building custom event

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component ,Input, Output,EventEmitter} from '@angular/core';
 
-const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
+
+
+
 
 @Component({
   selector: 'app-user',
@@ -12,15 +13,19 @@ const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({required: true}) id!: string;
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
+  @Output() select = new EventEmitter();
+
+
+  
   get imagePath(){
-    return 'assets/users/'+ this.selectedUser.avatar
+    return 'assets/users/'+ this.avatar;
   }
-
-  onSelectUser(){ 
-
-    const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+ 
+  onSelectUser(){
+    this.select.emit(this.id);
   }
-
+ 
 }
